9315777ed6127bd6a4ef7c085c1e09422fe076e7
icanbewill <akimbiwil@gmail.com>
2023-01-07 13:43:00 +0100

    Statistiques et sessions

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -6,7 +6,9 @@ use App\Entity\Category;
 use App\Entity\Property;
 use App\Entity\SearchedProperty;
 use App\Entity\User;
+use App\Repository\CategoryRepository;
 use App\Repository\PropertyRepository;
+use phpDocumentor\Reflection\Types\Object_;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,11 +18,11 @@ class DashboardController extends AbstractController
     /**
      * @Route("/dashboard", name="app_dashboard")
      */
-    public function index(PropertyRepository $propertyRepository): Response
+    public function index(CategoryRepository $categoryRepository): Response
     {
         $em = $this->getDoctrine()->getManager();
 
-        $properties = $propertyRepository->getSome();
+        // $properties = $propertyRepository->getSome();
 
         // totalUsers
         $repoUsers = $em->getRepository(User::class);
@@ -50,11 +52,40 @@ class DashboardController extends AbstractController
             ->getQuery()
             ->getSingleScalarResult();
 
+
+        $em = $this->getDoctrine()->getManager();
+
+        // Catégories les plus ajoutés favoris
+        $sql_categories_fav = 'SELECT i.category_id_id, COUNT(*) AS count
+        FROM favourite f
+        INNER JOIN property i ON f.bien_id_id = i.id
+        GROUP BY i.category_id_id 
+        ORDER BY count DESC LIMIT 5';
+        $statement_cat = $em->getConnection()->prepare($sql_categories_fav);
+        $categoriesGot = $statement_cat->execute()->fetchAll();
+        $categories = [];
+        foreach ($categoriesGot as $key => $value) {
+            $category = new Object_();
+            $category->libelle = $categoryRepository->findOneBy(array('id' => $value['category_id_id']))->getLibelle();
+            $category->count = $value['count'];
+            $categories[] = $category;
+        }
+
+        // Produits les plus ajoutés favoris
+        $sql_categories_property = 'SELECT i.*, COUNT(*) AS count
+        FROM favourite f
+        INNER JOIN property i ON f.bien_id_id = i.id
+        GROUP BY f.bien_id_id
+        ORDER BY count DESC LIMIT 5';
+        $statement_pro = $em->getConnection()->prepare($sql_categories_property);
+        $properties = $statement_pro->execute()->fetchAll();
+
         return $this->render('views/dashboard/index.html.twig', [
             'totalUser' => $totalUser,
             'totalProperties' => $totalProperties,
             'totalCategories' => $totalCategories,
             'totalResearches' => $totalResearches,
+            'categories' => $categories,
             'properties' => $properties,
         ]);
     }
